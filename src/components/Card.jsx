90f85e0019c2f4143539b8caf7cd4107fffc56de
Tomas Vargas <tavargas9@gmail.com>
2024-01-24 23:53:27 -0700

    clean porfolio page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,8 @@ import {
     Typography,
     Button,
   } from "@material-tailwind/react";
-import { faCode, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faCode, faArrowRight, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
    
   export default function CardDefault({ projects }) {
@@ -15,7 +16,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
             <h2 className="text-4xl h-10 font-semibold leading-7 bg-gradient-to-r from-orange-500 to-blue-500 inline-block text-transparent bg-clip-text">
                 Porfolio
             </h2>
-            <p className="text-lg font-semibold text-blue-900 mb-12">(Aug 2023-Present)</p>
+            <p className="text-lg font-semibold text-blue-900 mb-4">(Aug 2023 - Present)</p>
+            <button 
+            className='group flex items-center justify-center p-0.5 text-center font-medium relative focus:z-10 focus:outline-none text-white bg-gradient-to-r from-blue-500 to-orange-500 enabled:hover:bg-gradient-to-l focus:ring-blue-200 dark:focus:ring-blue-800 rounded-full focus:ring-2 mb-10'
+            onClick={() => window.open('https://github.com/tavargas9', '_blank')}
+            >
+                    <span className='items-center flex justify-center bg-white text-blue-900 transition-all duration-75 ease-in group-enabled:group-hover:bg-opacity-0 group-enabled:group-hover:text-inherit dark:bg-gray-900 dark:text-white w-full rounded-full text-sm px-4 py-2 border border-transparent'>
+                        <FontAwesomeIcon icon={faGithub} className="mr-2"/> View Full Profile <FontAwesomeIcon icon={faRightToBracket} className="ml-2"/>
+                    </span>
+                </button>
             <div className="flex flex-wrap gap-12 justify-evenly md:px-20">
             {projects.map((project) => (
                 <Card className="mt-6 w-96 hover:shadow-blue-800 transition ease-in">
